164c546db71ecdbd053297fe57f36a05aaaedf3c
nargin <92982124+nargin@users.noreply.github.com>
2024-09-06 12:46:57 +0200

    Start db not connecting...

--- src/components/app/Forms/RealEstateForm.tsx
+++ src/components/app/Forms/RealEstateForm.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import styles from './FormContainer.module.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import styles from "./FormContainer.module.css";
 
 const RealEstateForm = () => {
+  const [objectId, setObjectId] = useState("");
+
+  useEffect(() => {}, []);
+
   return (
     <div className={styles.formWrapper}>
       <h2>Real Estate Form</h2>
+      <select
+        name="realEstateType"
+        id="realEstateType"
+        onChange={(e) => setObjectId(e.target.value)}
+      >
+        <option value="buy">Buy</option>
+        <option value="sell">Sell</option>
+      </select>
+      {objectId && <p>Object ID: {objectId}</p>}
     </div>
   );
 };
